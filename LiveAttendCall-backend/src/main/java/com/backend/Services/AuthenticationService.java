0fae45507f20fbf70c4a31006d3aa516e8dcb6ec
Mkeyur1920 <keyurmistry19@gmail.com>
2025-02-08 09:14:05 +0530

    add auth service.

---
+++ LiveAttendCall-backend/src/main/java/com/backend/Services/AuthenticationService.java
@@ -0,0 +1,12 @@
+package com.backend.Services;
+
+import com.backend.Dto.*;
+
+public interface AuthenticationService {
+
+    RegisterResponse register(RegisterRequest registerRequest);
+
+    AuthResponse login(LoginRequest loginRequest);
+
+    void resetPassword(ResetPasswordRequest resetPasswordRequest);
+}
